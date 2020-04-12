8e1bbda04d94a868c4aece62704ef7a3c4d76099
Bernhard Häfner <bernhardhaefner@hotmail.com>
2020-04-12 09:06:12 +0200

    feat: Add first try surrogate function

--- agent.py
+++ agent.py
@@ -6,9 +6,11 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
+GAMMA = 0.99            # Discount factor
+LR = 5e-4               # Learning rate 
+EPSILON=0.1             # Epsilon
+BETA=0.01               # Discount for entropy
+CONST_INSTABILITIES = 1e-10     # Constant to avoid numerical instabilities
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -42,6 +44,59 @@ class Agent():
             eps (float): epsilon, for epsilon-greedy action selection
         """
 
+    def clipped_surrogate(self, old_probs, states, actions, rewards, 
+                          gamma=GAMMA, epsilon=EPSILON, beta=BETA):
+        """ Clipped surrogate function, adjusted from "PPO" lecture.
+
+        Params
+        ======
+            old_probs   List with probabilities according to old policy
+            states      List of states
+            actions     List of actions
+            rewards     List of rewards
+            gamma       Discount factor for rewards
+            epsilon     For epsilon-greedy policy
+            beta        Weight factor for entropy
+        """
+        
+        # Convert rewards to future rewards
+        discount = gamma**np.arange(len(rewards))
+        rewards = np.asarray(rewards)*discount[:,np.newaxis]
+        rewards_future = rewards[::-1].cumsum(axis=0)[::-1]
+
+        # Normalize rewards
+        mu = np.mean(rewards_future, axis=1)
+        std = np.std(rewards_future, axis=1) + CONST_INSTABILITIES
+        rewards_normalized = (rewards_future - mu[:,np.newaxis]) / std[:,np.newaxis]
+
+        # Convert everything into pytorch tensors and move to gpu if available
+        actions = torch.tensor(actions, dtype=torch.int8, device=device)
+        old_probs = torch.tensor(old_probs, dtype=torch.float, device=device)
+        rewards = torch.tensor(rewards_normalized, dtype=torch.float, device=device)
+
+        # Get new probabilities according to current policy
+        new_probs = self.policy(states)
+
+        # Ratio for clipping
+        ratio = new_probs/old_probs 
+        
+        # Clipping the function
+        clip = torch.clamp(ratio, 1-epsion, 1+epsilon)
+        clipped_surrogate = torch.min(ratio*rewards, clip*rewards)
+
+
+        # include a regularization term
+        # this steers new_policy towards 0.5
+        # add in 1.e-10 to avoid log(0) which gives nan
+        entropy = -(new_probs*torch.log(old_probs+CONST_INSTABILITIES)+ \
+                    (1.0-new_probs)*torch.log(1.0-old_probs+CONST_INSTABILITIES))
+
+
+        # this returns an average of all the entries of the tensor
+        # effective computing L_sur^clip / T
+        # averaged over time-step and number of trajectories
+        # this is desirable because we have normalized our rewards
+        return torch.mean(clipped_surrogate + beta*entropy)
 
     def save(self, filename):
         """Saves the agent to the local workplace
